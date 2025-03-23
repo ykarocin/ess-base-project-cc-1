69e30df387b0e1e5d0fafb94757c6df659419e07
Lucas Henrique <hns.lucass@gmail.com>
2025-03-23 15:51:25 -0300

    merge index of jalr and lhns3 and remove index of sales

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,8 +11,11 @@ app.use(cookieParser())
 // Route imports
 import userRoutes from './routes/users.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import filmesRoutes from './routes/filmes.routes.js'
+
 
 app.use('/users', userRoutes)
 app.use('/auth', authRoutes)
+app.use('/filmes', filmesRoutes)
 
 app.listen(4000);
